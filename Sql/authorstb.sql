4bfc881a87318d133050bac1944f0536030eabba
victor kimanga <vkimanga@gmail.com>
2023-09-28 21:23:54 +0300

    commit

---
+++ Sql/authorstb.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 10, 2023 at 05:13 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `authordb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authorstb`
+--
+
+CREATE TABLE `authorstb` (
+  `AuthorID` int(11) NOT NULL,
+  `AuthorFullName` text NOT NULL,
+  `AuthorEmail` text NOT NULL,
+  `AuthorAddress` text NOT NULL,
+  `AuthorBiography` longtext NOT NULL,
+  `AuthorDateofBirth` date NOT NULL,
+  `AuthorSuspend` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `authorstb`
+--
+ALTER TABLE `authorstb`
+  ADD PRIMARY KEY (`AuthorID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
